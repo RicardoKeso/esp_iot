1990be121dd23b6cf7dfecf756ae4719c33f4094
RicardoKeso <ricardo_keso@hotmail.com>
2018-09-21 17:57:27 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,22 +1,22 @@
 local Hardware = require "hardware";
-local Const = require "constantes";
 
 Hardware.clock("high");
-Hardware.configWiFi(Const.ssid(), Const.pssid());
-Hardware.configGPIO(Const.ledLog());
+Hardware.configWiFi("dlink-7238", "urrlg57003");
+Hardware.configGPIO();
 Hardware.adcInit();
 
 print("Aguardando IP...");
-Hardware.BlinkLed(1, 500, Const.ledLog());
+Hardware.BlinkLed(1, 500);
+
 tmr.alarm(0, 5000, 1, function()
     ipAddr = wifi.sta.getip();
     if ipAddr ~= nil then
         tmr.stop(0);
-        Hardware.BlinkLedStop(1, Const.ledLog());
+        Hardware.BlinkLedStop(1);
         print("");
         print("IP: "..ipAddr);
         print("SSID: \""..wifi.sta.getconfig().."\"\n");
-        
+        --print("start.lua - mem alocada: "..Hardware.memoriaAloc());
         dofile("principal.lua");
     end
 end)
