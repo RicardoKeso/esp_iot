95c36497332bd565f35660b820a0c9e9c57758c1
RicardoKeso <ricardo_keso@hotmail.com>
2018-09-21 13:43:56 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,18 +1,18 @@
 local Hardware = require "hardware";
-local ledLog = 4;
+local Const = require "constantes";
 
 Hardware.clock("high");
-Hardware.configWiFi("-", "-");
-Hardware.configGPIO(ledLog);
+Hardware.configWiFi(Const.ssid(), Const.pssid());
+Hardware.configGPIO(Const.ledLog());
 Hardware.adcInit();
 
 print("Aguardando IP...");
-Hardware.BlinkLed(1, 500, ledLog);
+Hardware.BlinkLed(1, 500, Const.ledLog());
 tmr.alarm(0, 5000, 1, function()
     ipAddr = wifi.sta.getip();
     if ipAddr ~= nil then
         tmr.stop(0);
-        Hardware.BlinkLedStop(1, ledLog);
+        Hardware.BlinkLedStop(1, Const.ledLog());
         print("");
         print("IP: "..ipAddr);
         print("SSID: \""..wifi.sta.getconfig().."\"\n");
